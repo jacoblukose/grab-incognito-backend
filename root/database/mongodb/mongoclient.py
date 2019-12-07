eb4bec2a4fd99b3c27388a186dee8199be94ba60
JACOB LUKOSE <jlukose@jlukose-mn1.linkedin.biz>
2019-12-08 03:18:37 +0530

    added mongo read

--- root/database/mongodb/mongoclient.py
+++ root/database/mongodb/mongoclient.py
@@ -3,14 +3,16 @@ from pymongo import MongoClient
 
 USERS_MONGO_COLLECTION = 'user_test'
 REFFERAL_MONGO_COLLECTION = 'referral_test'
+MONGO_CONNECTION_URL = 'localhost:27017'
 
 class MongoClientWrapper():
     def __init__(self,host='',port=''):
-            # mongo_configs = parse_yaml('root/configs/database-configs.yaml')
+
             #create mongoclient
-            client = MongoClient('localhost:27017')
+            client = MongoClient(MONGO_CONNECTION_URL)
             # select default database
             self.db = client['my']
+
             if USERS_MONGO_COLLECTION not in self.db.list_collection_names():
                 print("User collection not found. Creating...")
                 self.users = self.db[USERS_MONGO_COLLECTION]
@@ -20,8 +22,11 @@ class MongoClientWrapper():
                 self.users = self.db[REFFERAL_MONGO_COLLECTION]
 
     
-    def read():
-        pass
+    def read(self, query, collection_name):
+        collection = self.db[collection_name]
+        result=collection.find(query)
+        return result
+
 
     def write(self, data, collection_name):
         collection = self.db[collection_name]
@@ -33,4 +38,12 @@ class MongoClientWrapper():
 
 
 m = MongoClientWrapper()
-print(m.write({"name" : "Joe Drumgoole"}, 'user_test'))
+
+# write query sample...
+# print(m.write({"name" : "Joe Drumgoole"}, 'user_test'))
+
+# read query sample...
+# query = {'name':"Joe Drumgoole"}
+# data = m.read(query, 'user_test' )
+# for r in data:
+#     print(r)
