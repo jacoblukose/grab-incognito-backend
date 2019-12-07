e16df34c81bc2e2a6a0954322f755d9fad8b1502
JACOB LUKOSE <jlukose@jlukose-mn1.linkedin.biz>
2019-12-08 03:04:16 +0530

    fixes in mongo code

--- root/database/mongodb/mongoclient.py
+++ root/database/mongodb/mongoclient.py
@@ -1,16 +1,16 @@
 
 from pymongo import MongoClient
 
-USERS_MONGO_COLLECTION = 'users_test'
-REFFERAL_MONGO_COLLECTION = 'referral'
+USERS_MONGO_COLLECTION = 'user_test'
+REFFERAL_MONGO_COLLECTION = 'referral_test'
 
 class MongoClientWrapper():
     def __init__(self,host='',port=''):
             # mongo_configs = parse_yaml('root/configs/database-configs.yaml')
+            #create mongoclient
             client = MongoClient('localhost:27017')
             # select default database
             self.db = client['my']
-            print(self.db.list_collection_names())
             if USERS_MONGO_COLLECTION not in self.db.list_collection_names():
                 print("User collection not found. Creating...")
                 self.users = self.db[USERS_MONGO_COLLECTION]
